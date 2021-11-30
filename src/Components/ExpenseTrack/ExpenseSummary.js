ffd451dd14ca7007119fe7cb459a4d8b6fda123a
GiovaneForlenza <giovane.franciscoforl@gmail.com>
2021-11-30 16:29:59 -0500

    update on Expense Summary

--- src/Components/ExpenseTrack/ExpenseSummary.js
+++ src/Components/ExpenseTrack/ExpenseSummary.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../../Style/Expenses-Track/expenseSummary.scss";
 
 function ExpenseSummary() {
-  return <div className="expenseResumeContainer">Resumeaaa</div>;
+  return <div className="expenseResumeContainer">Resume</div>;
 }
 
 export default ExpenseSummary;
